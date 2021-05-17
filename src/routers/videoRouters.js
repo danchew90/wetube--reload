c32799b56510000ae1aee70055bc2f4b3202521e
danchew90 <danchew90@naver.com>
2021-05-18 00:14:35 +0900

    18.1 Array Database part Two (절대경로 상대경로)

--- src/routers/videoRouters.js
+++ src/routers/videoRouters.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { see, edit, deleteVideo, upload } from "../controllers/videoController";
+import {
+  watch,
+  edit,
+  deleteVideo,
+  upload,
+} from "../controllers/videoController";
 
 const viedoRouter = express.Router();
 
-viedoRouter.get("/:id(\\d+)", see);
+viedoRouter.get("/:id(\\d+)", watch);
 viedoRouter.get("/:id(\\d+)/edit", edit);
 viedoRouter.get("/:id(\\d+)/delete", deleteVideo);
 viedoRouter.get("/upload", upload);
