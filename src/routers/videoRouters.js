f823d095dcf3d5223d3b9e62c66159d133dad299
danchew90 <danchew90@naver.com>
2021-05-18 00:46:08 +0900

    18.2 Edit Video part One

--- src/routers/videoRouters.js
+++ src/routers/videoRouters.js
@@ -1,16 +1,16 @@
 import express from "express";
 import {
   watch,
-  edit,
   deleteVideo,
   upload,
+  getEdit,
+  postEdit,
 } from "../controllers/videoController";
 
 const viedoRouter = express.Router();
 
 viedoRouter.get("/:id(\\d+)", watch);
-viedoRouter.get("/:id(\\d+)/edit", edit);
-viedoRouter.get("/:id(\\d+)/delete", deleteVideo);
-viedoRouter.get("/upload", upload);
+viedoRouter.get("/:id(\\d+)/edit", getEdit);
+viedoRouter.post("/:id(\\d+)/edit", postEdit);
 
 export default viedoRouter;
