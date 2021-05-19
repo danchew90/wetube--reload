673cba316ec76d2d191a3ea6a8b6245cce01eb0a
danchew90 <danchew90@naver.com>
2021-05-19 18:19:43 +0900

    18.5 More Practice part One

--- src/routers/videoRouters.js
+++ src/routers/videoRouters.js
@@ -1,15 +1,16 @@
 import express from "express";
 import {
   watch,
-  deleteVideo,
-  upload,
+  getUpload,
   getEdit,
   postEdit,
+  postUpload,
 } from "../controllers/videoController";
 
 const viedoRouter = express.Router();
 
 viedoRouter.get("/:id(\\d+)", watch);
 viedoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+viedoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default viedoRouter;
