7d02ba8cfdeec13598e9425515f690e567261d33
danchew90 <danchew90@naver.com>
2021-05-24 23:36:49 +0900

    6.25 Delete Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,7 +5,7 @@ import Video from "../models/video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
-  console.log(videos);
+  // console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = async (req, res) => {
@@ -57,4 +57,11 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  // delete video
+  return res.redirect("/");
 };
--- src/routers/videoRouters.js
+++ src/routers/videoRouters.js
@@ -5,12 +5,14 @@ import {
   getEdit,
   postEdit,
   postUpload,
+  deleteVideo,
 } from "../controllers/videoController";
 
 const viedoRouter = express.Router();
 
 viedoRouter.get("/:id([0-9a-f]{24})", watch);
 viedoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+viedoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 viedoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default viedoRouter;
