7a4e16357c01ad4aae4a15fce0f07735aba9aa0a
danchew90 <danchew90@naver.com>
2021-08-05 01:01:16 +0900

    #7.7 Sessions and Cookies part One

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import viedoRouter from "./routers/videoRouters";
 import userRouter from "./routers/userRouters";
@@ -18,17 +19,17 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
 app.use("/", rootRouter);
 app.use("/videos", viedoRouter);
 app.use("/users", userRouter);
-
-const home = (req, res) => {
-  console.log("I will respond.");
-  return res.send("Hello");
-};
-
-const login = (req, res) => {
-  return res.send("login");
-};
 
 export default app;
