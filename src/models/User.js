4be53a033b520e6e6929b6ed415f2a36747954a5
danchew90 <danchew90@naver.com>
2021-08-07 23:10:03 +0900

    #7.22 Log Out

--- src/models/User.js
+++ src/models/User.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true }, //unique는 다른 user와 중복되지않고 한명만 가지게 하는 요소
+  avatarUrl: String,
   socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
   password: { type: String },
