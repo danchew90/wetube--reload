e9a975f0e19509a341971dad00b51291d9362170
danchew90 <danchew90@naver.com>
2021-08-09 23:14:17 +0900

    #8.5 Change Password part Two

--- src/models/User.js
+++ src/models/User.js
@@ -12,9 +12,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function () {
-  console.log("Users password:", this.password);
   this.password = await bcrypt.hash(this.password, 5);
-  console.log("Hash password:", this.password);
 });
 const User = mongoose.model("User", userSchema);
 export default User;
